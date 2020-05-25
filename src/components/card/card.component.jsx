093d988797f5ae0026ce82429643aa39f065b783
Raphael Fernandes <raph@MacBook-Pro-de-Raphael-2.local>
2020-05-25 10:05:34 +0200

    retry

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,6 +6,6 @@ export const Card = (props) => (
     <div className='card-container'>
         <img alt="monster" src={`https://robohash.org/${props.monster.id}.?set=set2`} />
         <h2> {props.monster.name}</h2>
-        <p> {props.monster.email}</p>
+        <p> {props.monster.email} </p>
     </div>
 ); 
